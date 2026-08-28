43d36afcfd5178952587d29de4cb82a5e7cc4132
SweetBlue16 <mauricionoriega.sw@gmail.com>
2026-08-28 16:55:35 -0600

    feat: add CWE-476 practice

---
+++ PracticaCWE-476/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/alumno', (req, res) => {
+    if (req.body.id) {
+        res.send(`Hola ${alumnos[req.body.id - 1].nombre} ${alumnos[req.body.id - 1].apellido}`);
+    } else {
+        res.send('No se ha especificado el ID del alumno');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Aplicación de ejemplo escuchando en el puerto ${port}`);
+});
+
+const alumnos = [
+    { nombre: "Pedro", apellido: "Perez" },
+    { nombre: "Rosa", apellido: "Vazquez" },
+    { nombre: "Susana", apellido: "Ochia" }
+];
+
